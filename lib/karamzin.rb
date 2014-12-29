100bf848a0c62cbf52269fea4f45136f6078e784
pavel <kalashnikovisme@gmail.com>
2014-12-29 03:42:46 +0300

    update search

--- lib/karamzin.rb
+++ lib/karamzin.rb
@@ -6,9 +6,19 @@ module Karamzin
   include Config
 
   def insert(str)
-    #dictionary = YamlLoader.yaml_object 'dictionaryE'
-    words = str.split
-    filter_words words
+    @dictionaryE = YamlLoader.yaml_object('dictionaryE')[:words]
+    words = filter_words str.split
+    dic_words = []
+    words.map do |word|
+      if is_in_dictionary? word
+        dic_words << word
+      end
+    end
+    dic_words
+  end
+
+  def is_in_dictionary?(word)
+    @dictionaryE.include? word.mb_chars.downcase.wrapped_string
   end
 
   def filter_words(words)
