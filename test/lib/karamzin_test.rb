09bc96f229f16c826189ddd4357c37254a335cbc
pavel <kalashnikovisme@gmail.com>
2014-12-29 17:07:57 +0300

    add test with big text

--- test/lib/karamzin_test.rb
+++ test/lib/karamzin_test.rb
@@ -7,5 +7,10 @@ class KaramzinTest < TestCase
     assert_equal 'Ёлки, палки', Karamzin.insert('Елки, палки')
     assert_equal 'В лесу родилась ёлочка!', Karamzin.insert('В лесу родилась елочка!')
     assert_equal 'В лесу родилась ёлочка ёлочка!', Karamzin.insert('В лесу родилась елочка елочка!')
+    str = ''
+    (0..1000).each do
+      str += 'В лесу родилась елочка! '
+    end
+    assert Karamzin.insert(str)
   end
 end
