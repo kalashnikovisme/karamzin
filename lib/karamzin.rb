4c3d67908bd6b6301de37d517a26cb4fdcf5fe28
pavel <kalashnikovisme@gmail.com>
2014-12-30 08:24:48 +0300

    update speed

--- lib/karamzin.rb
+++ lib/karamzin.rb
@@ -1,20 +1,21 @@
 require 'karamzin/version'
 require 'karamzin/config'
 require 'yaml'
+#require 'active_support/core_ext'
 
 module Karamzin
   include Config
 
   def insert(str)
-    @dictionaryE = YamlLoader.yaml_object('dictionaryE')[:words]
-    @dictionary = YamlLoader.yaml_object('dictionary')[:words]
+    make_dictionaries
     words = filter_words str.split
     paste_words = []
-    words.each_with_index do |word, i|
-      if is_in_dictionary word
+    words.each do |word|
+      index_in_dictionary = is_in_dictionary word
+      if index_in_dictionary
         paste_words << {
           replace_word: word,
-          paste_word: @dictionary[is_in_dictionary(word)]
+          paste_word: @dictionary[first_letter_in(word)][index_in_dictionary]
         }
       end
     end
@@ -26,8 +27,40 @@ module Karamzin
     str
   end
 
+  def first_letter_in(word)
+    letter = word[0].mb_chars.downcase.to_s
+    letter == 'ё' ?  'е' : letter
+  end
+
   def is_in_dictionary(word)
-    @dictionaryE.index word.mb_chars.downcase.wrapped_string
+    if @dictionaryE[word[0].mb_chars.downcase.to_s]
+      if word[0] == 'ё'
+        word[0] = 'e'
+        @dictionaryE['е'].index word.mb_chars.downcase.wrapped_string
+      else
+        @dictionaryE[word[0].mb_chars.downcase.to_s].index word.mb_chars.downcase.wrapped_string
+      end
+    end
+  end
+
+  def make_dictionaries
+    @dictionary = make_dictionary 'dictionary'
+    @dictionaryE = make_dictionary 'dictionaryE'
+  end
+
+  def make_dictionary(name)
+    dictionary = YamlLoader.yaml_object(name)[:words]
+    variable = {}
+    dictionary.each do |word|
+      if word[0] == 'ё'
+        variable['е'] ||= []
+        variable['е'] << word
+      else
+        variable[word[0]] ||= []
+        variable[word[0]] << word
+      end
+    end
+    variable
   end
 
   def equate_words_register(wordE, word)
